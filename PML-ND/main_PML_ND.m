fa2f0791cdecee6fc1df52332b27d9d7449d8d68
Jingyu Zhong <93188158+zhongjingyu1@users.noreply.github.com>
2024-12-18 10:20:14 +0800

    Update main_PML_ND.m

--- PML-ND/main_PML_ND.m
+++ PML-ND/main_PML_ND.m
@@ -4,51 +4,6 @@ addpath(genpath('.'));
 load('data/mirflickr.mat')
 ttt=1;
 params = [2^-6,    2^-2,   2^8,   2^0   0.1;     % 1.mirflickr
-          2^-4,   2^-10,   2^-8,   2^0    1;     % 2.music_emotion
-          2^0,  2^-10,   2^-10,  2^4   1000;     % 3.music_style
-          2^-2,   2^10,   2^-4,   2^-10   1;     % 4.PML_emotions_2
-          2^0,   2^10,   2^-4,   2^-6   0.1;     % 5.PML_emotions_3
-          2^0,   2^10,  2^-10,  2^-10   0.1;     % 6.PML_emotions_4
-          2^-4,   2^10,   2^0,    2^0   0.1;     % 7.PML_emotions_5
-          2^2,   2^10,   2^-10,   2^0   0.1;     % 8.PML_medical_2
-          2^4,   2^10,   2^-2,    2^0   0.1;     % 9.PML_medical_3
-          2^4,   2^4,   2^-10,    2^0   100;     % 10.PML_medical_4
-          2^2,   2^-4,   2^-10,   2^0   100;     % 11.PML_medical_5
-          2^-10,   2^-2,  2^-10, 2^-10  0.1;     % 12.PML_health_5
-          2^-8,   2^0,   2^-8,   2^-10  0.1;     % 13.PML_health_7
-          2^-10,  2^-10, 2^-8,  2^-10   0.1;     % 14.PML_health_9          
-          2^-10,   2^8,   2^-8,  2^-10  0.1;     % 15.PML_recreation_5
-          2^-10,  2^8, 2^-10,   2^-2    0.1;     % 16.PML_recreation_7
-          2^-10,  2^2, 2^-10,   2^-10   0.1;     % 17.PML_recreation_9
-          2^-10,  2^10, 2^-10,   2^0    0.1;     % 18.PML_flags_4
-          2^-2,  2^10, 2^-10,   2^-6    0.1;     % 19.PML_flags_5
-          2^-10,  2^10, 2^-10,   2^0    0.1;     % 20.PML_education_5
-          2^-4,  2^8, 2^-8,   2^-10     0.1;     % 21.PML_education_7
-          2^-8,  2^0, 2^-10,   2^-6     0.1;     % 22.PML_education_9
-          2^-10,  2^8, 2^-6,   2^-6     0.1;     % 23.PML_science_5
-          2^-8,  2^8, 2^-10,   2^-10    0.1;     % 24.PML_science_7
-          2^-8,  2^-8, 2^-8,   2^-10    0.1;     % 25.PML_science_9
-          2^-8,  2^-8, 2^-8,   2^-10    0.1;     % 26.PML_genbase_2
-          2^-8,  2^-8, 2^-8,   2^-10    0.1;     % 27.PML_genbase_3
-          2^-8,  2^-8, 2^-8,   2^-10    0.1;     % 28.PML_genbase_4
-          2^-8,  2^-8, 2^-8,   2^-10    0.1;     % 29.PML_genbase_5
-          2^-10,  2^-8, 2^-6,   2^-10   0.1;     % 30.PML_Scene_2
-          2^-10,  2^-8, 2^-6,   2^-10   0.1;     % 31.PML_Scene_3
-          2^-10,  2^-8, 2^-6,   2^-10   0.1;     % 32.PML_Scene_4
-          2^-6,  2^2, 2^-10,   2^-10    0.1;     % 33.PML_Scene_5
-          2^-6,  2^2, 2^-10,   2^-10    0.1;     % 34.PML_Bibtex_4
-          2^10,   2^2,  2^0,    2^2     0.1;     % 35.PML_Yeast_5
-          2^10,    2^4,   2^2,    2^2   0.1;     % 36.PML_Yeast_7
-          2^8,    2^0,   2^0,    2^0    100;     % 37.PML_Yeast_9
-          2^-10,    2^4,   2^0,   2^2   100;     % 38.PML_art_5
-          2^-6,    2^4,   2^-6,    2^-4   1;     % 39.PML_art_7
-          2^-6,    2^0,  2^-10,  2^-8   0.1;     % 40.PML_art_9
-          2^10,    2^6,     2^2,  2^2    10;     % 41.PML_Yeast_5
-          2^10,    2^6,  2^0,   2^-10   100;     % 42.PML_Yeast_7
-          2^10,   2^6,  2^-10,   2^-10   100;     % 43.PML_Yeast_9
-          2^-2,  2^10, 2^-10,   2^-6    1;     % 44.PML_flags_6
-
-
 ];
 partial_labels=candidate_labels;
 beta=params(ttt,1);
@@ -79,7 +34,7 @@ for i=1:nfold%
     train_p_target = partial_labels(:,train_ind);
     Truth_label1 = Truth_label(:,train_ind);
     test_data = data(test_ind,:);
-    test_target = Truth_label(:, test_ind);
+    test_target = target(:, test_ind);
 
     tic
     [W,obj] =PML_ND(train_data,train_p_target',Truth_label1',beta,lambda,gamma,delta,enaf);
